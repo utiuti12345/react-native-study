08153fbd0736c50df376ad1b5ecbb91c5b9d2332
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-02-05 00:04:15 +0900

    user_infoにはDrawerNavigationを表示しない修正を追加

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import {createStackNavigator,StackCardInterpolationProps} from "@react-navigation/stack";
+import {createStackNavigator, StackCardInterpolationProps} from "@react-navigation/stack";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {createDrawerNavigator} from "@react-navigation/drawer";
+import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
 import * as UiContext from "../../contexts/ui";
 import {CHOOSE_LOGIN, HOME, INITIAL, LOADING, STATISTICS, USER_INFO} from "../../constants/path";
 import {ChooseLogin, Initial, Loading, UserInfo} from "../../components/pages";
@@ -12,14 +13,25 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const HomeDrawer = createDrawerNavigator();
 const StatisticsDrawer = createDrawerNavigator();
-const forFade = ({current}:StackCardInterpolationProps) => ({
-   cardStyle:{
-       opacity:current.progress,
-   }
+const forFade = ({current}: StackCardInterpolationProps) => ({
+    cardStyle: {
+        opacity: current.progress,
+    }
 });
 
+const getActiveRouteName = (state: any): string => {
+    if (!state || !state.routes) {
+        return '';
+    }
+    const route = state.routes[state.index];
+    if (route.state) {
+        return getActiveRouteName(route.state);
+    }
+    return route.name;
+};
+
 function HomeWithDrawer() {
-    return(
+    return (
         <HomeDrawer.Navigator initialRouteName={HOME}>
             <HomeDrawer.Screen name={HOME} component={Home}/>
             <HomeDrawer.Screen name={USER_INFO} component={UserInfo}/>
@@ -28,7 +40,7 @@ function HomeWithDrawer() {
 }
 
 function StatisticsWithDrawer() {
-    return(
+    return (
         <StatisticsDrawer.Navigator initialRouteName={STATISTICS}>
             <StatisticsDrawer.Screen name={STATISTICS} component={Statistics}/>
             <StatisticsDrawer.Screen name={USER_INFO} component={UserInfo}/>
@@ -37,22 +49,27 @@ function StatisticsWithDrawer() {
 }
 
 function TabRoutes() {
-    return(
-        <Tab.Navigator initialRouteName={HOME}>
+    return (
+        <Tab.Navigator initialRouteName={HOME} screenOptions={(props: any) => {
+            const routeName = getFocusedRouteNameFromRoute(props.route);
+            return {
+                tabBarVisible: routeName !== USER_INFO
+            }
+        }}>
             <Tab.Screen name={HOME} component={HomeWithDrawer}/>
             <Tab.Screen name={STATISTICS} component={StatisticsWithDrawer}/>
         </Tab.Navigator>
     )
 }
 
-function switchingAuthStatus(status:UiContext.Status) {
+function switchingAuthStatus(status: UiContext.Status) {
     switch (status) {
         case UiContext.Status.UN_AUTHORIZED:
             return <Stack.Screen name={CHOOSE_LOGIN} component={ChooseLogin}/>;
         case UiContext.Status.AUTHORIZED:
             return <Stack.Screen name={HOME} component={TabRoutes}/>;
         default:
-            return <Stack.Screen name={INITIAL} component={Initial} />
+            return <Stack.Screen name={INITIAL} component={Initial}/>
     }
 }
 
@@ -61,11 +78,11 @@ function AuthWithRoutes() {
         UiContext.Context
     );
 
-    return(
-        <Stack.Navigator initialRouteName={LOADING} headerMode="none" screenOptions={{cardStyleInterpolator:forFade}}>
+    return (
+        <Stack.Navigator initialRouteName={LOADING} headerMode="none" screenOptions={{cardStyleInterpolator: forFade}}>
             {uiContext.applicationState !== UiContext.Status.LOADING ? (
                 switchingAuthStatus(uiContext.applicationState)
-            ):(
+            ) : (
                 <Stack.Screen name={LOADING} component={Loading}/>
             )}
         </Stack.Navigator>
