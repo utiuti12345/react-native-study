60d2fcea4c6116eebb219e380b7db1d8bf0feaab
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-02-11 23:06:44 +0900

    Choose_login画面を修正

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {Image, ImageSourcePropType, ImageStyle, StyleSheet} from "react-native";
+import {width} from "../../lib/window";
+import reactImage from "../../../assets/reactIcon.jpg";
+
+const edgeNumber = 2;
+const ratio = 3;
+const imageRatio = edgeNumber / ratio;
+
+const styles = StyleSheet.create({
+    image:{
+        width:width * imageRatio,
+        flex:1,
+        resizeMode:'contain',
+    }
+});
+
+interface Props {
+    image?:ImageSourcePropType;
+    style?:ImageStyle | ImageStyle[];
+}
+
+export default function Logo(props:Props) {
+    const {image = reactImage,style} = props;
+    return <Image source={image} resizeMode="contain" style={[styles.image,style]}/>
+}
--- src/components/pages/ChooseLogin/index.tsx
+++ src/components/pages/ChooseLogin/index.tsx
@@ -2,12 +2,30 @@ import React from "react";
 import {useNavigation} from '@react-navigation/native';
 import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {SIGN_IN, SIGN_UP} from "../../../constants/path";
+import {Button, Logo} from "../../atoms";
+import {COLOR} from "../../../constants/theme";
 
+const padding = 20;
 const styles = StyleSheet.create({
     container:{
         flex:1,
+        backgroundColor:COLOR.MAIN,
+    },
+    imageContainer:{
+        flex: 1,
         justifyContent:'center',
-        alignItems:'center',
+        alignItems: 'center',
+    },
+    contentContainer:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems: 'center',
+        paddingBottom:40,
+        paddingVertical:padding,
+    },
+    button:{
+        marginBottom:40,
+        width:300,
     }
 });
 
@@ -15,13 +33,15 @@ export default function ChooseLogin() {
     const { navigate } = useNavigation();
     return(
         <View style={styles.container}>
-            <TouchableOpacity onPress={() => navigate(SIGN_IN)}>
-                <Text>Go To SignIn</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigate(SIGN_UP)}>
-                <Text>Go To SignUp</Text>
-            </TouchableOpacity>
-            <Text>ChooseLogin</Text>
+            <View style={styles.imageContainer}>
+                <Logo/>
+            </View>
+            <View style={styles.contentContainer}>
+                <Button onPress={() => navigate(SIGN_IN)} style={styles.button} label="Sign in"/>
+            </View>
+            <View style={styles.contentContainer}>
+                <Button onPress={() => navigate(SIGN_UP)} style={styles.button} label="Sign up"/>
+            </View>
         </View>
     )
 }
