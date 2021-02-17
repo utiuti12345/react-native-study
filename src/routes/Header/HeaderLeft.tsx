64726ab32365192e8b312efe841e3d617dc6dfb9
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-02-18 00:40:38 +0900

    Drawer Navigationの変更

--- src/routes/Header/HeaderLeft.tsx
+++ src/routes/Header/HeaderLeft.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {useNavigation} from '@react-navigation/native';
 import {DrawerActions} from '@react-navigation/routers';
 import {Text, TouchableOpacity} from "react-native";
+import {COLOR} from "../../constants/theme";
 
-export default function HeaderLeft() {
+function HeaderLeft() {
     const {dispatch} = useNavigation();
     const onPress = React.useCallback(() => {
         dispatch(DrawerActions.openDrawer())
     },[dispatch]);
 
     return(
-        <TouchableOpacity onPress={onPress}>
-            <Text>Open</Text>
-        </TouchableOpacity>
+        <Icon.Button name="bars" color={COLOR.WHITE} backgroundColor={COLOR.MAIN} onPress={onPress}/>
     )
 }
+
+export default HeaderLeft;
